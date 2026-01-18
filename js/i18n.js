b8735df64d8e4f07cace49dd4979a24478cf7139
Renato <renatomartsh@gmail.com>
2026-01-18 18:00:36 -0300

    comentado js

--- js/i18n.js
+++ js/i18n.js
@@ -1,14 +1,10 @@
 async function loadLanguage(lang) {
   //alert(lang);
   const response = await fetch(`lang/${lang}.json`);
-  alert(response);  
   const translations = await response.json();
-  alert(translations)
   document.querySelectorAll("[data-i18n]").forEach(el => {
     const key = el.getAttribute("data-i18n");
-    alert(key);
     el.textContent = translations[key] || key;
-    alert(translations[key]);
   });
 
   document.documentElement.lang = lang;
